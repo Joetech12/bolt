c8e830811380baeaf8797786cecd9af6f5996578
Ifeanyi Umeh <ifeanyi4umeh@gmail.com>
2022-10-07 01:50:31 +0100

    updated search

--- components/Header.js
+++ components/Header.js
@@ -43,7 +43,7 @@ const Header = () => {
             <input
               ref={searchInputRef}
               type="text"
-              className="focus:outline-none flex-1 rounded-[50px] pl-[5px] md:px-[20px]"
+              className="focus:outline-none rounded-[50px] pl-[5px] md:px-[20px] w-[140px]"
             />
             <AiOutlineClose
               className="h-10 w-5 text-gray-500 cursor-pointer mx-[5px] mr-[0px] md:mr-[0px] transition duration-100 transform hover:scale-105"
@@ -51,7 +51,7 @@ const Header = () => {
             />
             <TbMinusVertical className="h-10 w-5 text-gray-300 cursor-pointer mx-[0px] md:mx-[2px] " />
             <IoMdMic className="h-10 w-5 text-green-700 cursor-pointer mx-[0px] md:mx-[5px] hidden md:flex" />
-            <BiSearch className="h-10 w-5 ml-[0px] md:ml-[10px] mr-[15px] text-gray-500 cursor-pointer" />
+            <BiSearch className="h-10 w-5 ml-[0px] md:ml-[10px] mr-[2px] md:mr-[15px] text-gray-500 cursor-pointer" />
             <button hidden type="submit" onClick={search}>
               Search
             </button>
